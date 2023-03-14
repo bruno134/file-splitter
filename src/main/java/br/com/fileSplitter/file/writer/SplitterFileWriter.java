2f70c5bd4b434870315b5d9b9de1d19887d12fa6
bruno134 <brunodn@gmail.com>
2023-03-14 17:30:01 -0300

    Added file creation functionality

--- src/main/java/br/com/fileSplitter/file/writer/SplitterFileWriter.java
+++ src/main/java/br/com/fileSplitter/file/writer/SplitterFileWriter.java
@@ -1,6 +1,7 @@
 package br.com.fileSplitter.file.writer;
 
 import java.io.File;
+import java.io.IOException;
 
 public class SplitterFileWriter {
 
@@ -15,14 +16,14 @@ public class SplitterFileWriter {
 		return null;
 	}
 
-	public String writeAndSaveFile(String fileName, StringBuilder stringBuilder, Integer firstLine, Integer lastLine) {
+	public String writeAndSaveFile(String fileName, StringBuilder stringBuilder, Integer firstLine, Integer lastLine) throws FileWriterException {
 		
 		File file = createFile(fileName);
 
 		return writeAndSaveFile(file, stringBuilder, firstLine, lastLine);
 	}
 
-	public String writeAndSaveFile(StringBuilder stringBuilder, Integer firstLine, Integer lastLine) {
+	public String writeAndSaveFile(StringBuilder stringBuilder, Integer firstLine, Integer lastLine) throws FileWriterException {
 
 		final String fileName = String.format("File_%d%d_%d.txt", firstLine, lastLine, System.currentTimeMillis());
 		
@@ -32,9 +33,18 @@ public class SplitterFileWriter {
 		
 	}
 
-	public File createFile(String fileName) {
+	public File createFile(String fileName) throws FileWriterException {
 
-		File file =  new File(fileName);
+		try {
+			File file =  new File(fileName);
+			
+			file.createNewFile();
+			
+		} catch (IOException e) {
+			throw new  FileWriterException("Failed to create the file");
+		} catch (NullPointerException e) {
+			throw new  FileWriterException("File path not found or invalid");
+		}
 		
 		return null;
 	}

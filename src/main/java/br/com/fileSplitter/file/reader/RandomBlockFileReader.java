15de473b7ae5f9748169b4cedaee1d409184b818
bruno134 <brunodn@gmail.com>
2023-03-16 13:39:08 -0300

    Random Reader class created Random Reader tests created

---
+++ src/main/java/br/com/fileSplitter/file/reader/RandomBlockFileReader.java
@@ -0,0 +1,6 @@
+package br.com.fileSplitter.file.reader;
+
+public class RandomBlockFileReader {
+	
+	
+}
---
+++ src/test/java/br/com/fileSplitter/file/reader/RandomBlockFileReaderTest.java
@@ -0,0 +1,42 @@
+package br.com.fileSplitter.file.reader;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RandomBlockFileReaderTest {
+
+	
+	@Test
+	void shouldReturnTextBlockWhenLinesPointerAndFileAreProvided() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void shouldRaiseErrorWhenFileNameIsNotGiven() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void shouldRaiseErrorWhenGivenFileIsNotFound() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void shouldRaiseErrorWhenFileIsRestricted() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void shouldRaiseErrorWhenFirstPointerNameIsNotGiven() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void shouldRaiseErrorWhenLastPointerNameIsNotGiven() {
+		fail("Not yet implemented");
+	}
+	
+	
+
+}

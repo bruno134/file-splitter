141a7e6cc50bc6f8f1595530580f98de8ae0824a
bruno134 <brunodn@gmail.com>
2023-03-24 07:42:58 -0300

    created splitter main class

---
+++ src/main/java/br/com/fileSplitter/Splitter.java
@@ -0,0 +1,13 @@
+package br.com.fileSplitter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import br.com.fileSplitter.file.SplitterFileConfiguration;
+
+public class Splitter {
+
+	private static final Logger LOG = LoggerFactory.getLogger(Splitter.class);
+	private SplitterFileConfiguration config;
+	
+}
